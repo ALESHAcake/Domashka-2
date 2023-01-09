ba664b2c442df4e626f8427bd1d7bb95e2c1c00b
aleksey <alekseymalinovskiy@gmail.com>
2023-01-09 18:22:05 +0300

    Задание первое

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int Num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int Temp = Num;
+int Rev = 0;
+while (Temp > 0)
+{
+    Rev *= 10;
+    Rev += Temp % 10;
+    Temp /= 10;
+}
+if (Rev == Num)
+{
+    Console.WriteLine("О чудо это палиндром!");
+}
+else
+{
+    Console.WriteLine("Это обычное число, чуда не будет.");
+}
